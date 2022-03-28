d4b0902f23e2d8de91ecd356c5154f4b2efb818a
Luke Gloege <lukegloege@gmail.com>
2022-03-27 22:32:11 -0400

    Add upload_car() method

--- src/web3storage/web3storage.py
+++ src/web3storage/web3storage.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import subprocess
 import requests
 import json
 import os
@@ -9,6 +10,76 @@ def json_print(contents):
     json_object = json.loads(contents)
     json_formatted_str = json.dumps(json_object, indent=2)
     print(json_formatted_str)
+
+
+def create_car(source_dir, output_file):
+    """creates a car file
+    this requires ipfs-car command line tool
+    (see https://web3.storage/docs/how-tos/work-with-car-files)
+
+    install ipfs-caf:
+     ```npm install -g ipfs-car```
+
+     args:
+     ------
+     source_dir (str): the directory with files to upload
+     output_file (str): the directory where car file will be stored
+
+     returns:
+     -----
+     output_dict: dictionary with CID and output path
+    """
+    # make sure source directory exists
+    source_dir = os.path.expanduser(source_dir)
+    source_obj = Path(source_dir)
+    if not source_obj.exists():
+        raise FileNotFoundError(f"{source_dir} does not exist")
+
+    # acceptable extensions for outputfile
+    acceptable_extensions = ['.car']
+
+    # use name of source_dir for output_file if none is included
+    if not output_file:
+        output_file = f"{source_obj.stem}.car"
+        output_obj = Path(output_file)
+    else:
+        output_file = os.path.expanduser(output_file)
+        output_obj = Path(output_file)
+        # gets the file extentions
+        extension = ''.join(output_obj.suffixes)
+
+        # make sure extension is acceptable
+        if extension not in acceptable_extensions:
+            raise Exception(f"Extension must be in {acceptable_extensions}")
+
+        # add an extension if not included
+        if not extension:
+            output_file = os.path.expanduser(output_file + '.car')
+            output_obj = Path(output_file)
+
+    # check to make sure outputfile doesn't already exist
+    if output_obj.exists():
+        raise Exception(f"{output_obj} already exists. Please chance the name")
+
+    # create tar directory if does not exist
+    if output_obj.parent.exists():
+        # make car file
+        command = f"ipfs-car --pack {source_dir} --output {output_file}"
+        make_car = subprocess.run(command.split(' '), stdout=subprocess.PIPE)
+        output = make_car.stdout.decode("utf-8").split('\n')
+        output_dict = {output[0].split(':')[0].strip(): output[0].split(':')[1].strip(),
+                       output[1].split(':')[0].strip(): output[1].split(':')[1].strip()}
+    else:
+        # create directory
+        os.makedirs(output_obj.parent)
+        # make car file
+        command = f"ipfs-car --pack {source_dir} --output {output_file}"
+        make_car = subprocess.run(command.split(' '), stdout=subprocess.PIPE)
+        output = make_car.stdout.decode("utf-8").split('\n')
+        output_dict = {output[0].split(':')[0].strip(): output[0].split(':')[1].strip(),
+                       output[1].split(':')[0].strip(): output[1].split(':')[1].strip()}
+
+    return output_dict
 
 
 class BearerAuth(requests.auth.AuthBase):
@@ -122,12 +193,13 @@ class Client(object):
     # user facing methods
     # ---------------------------------------------
 
-    def upload(self, file_path=None):
+    def upload(self, file_path=None, filename=None):
         """Store files using Web3.Storage. 
         You can upload either a single file or multiple files.
 
         Args:
-            filename (str): name of the file to download
+            file_path (str): path to file to uploade
+            filename (str): filename that will appear in web3.storage
         """
         if file_path is None:
             print("You need to supply a path")
@@ -136,11 +208,45 @@ class Client(object):
             print(f"{file_path} does not exist. Please check you entered the correct path")
         else:
             #data = {"file": open(file_path, "rb")}
+
+            # supply a filename
+            headers = {"X-NAME": filename.encode(encoding='utf-8')}
+
             with open(file_path, "rb") as fp:
                 r = requests.post(self._endpoint + '/upload',
                                   auth=self._bearer_auth,
-                                  data=fp)
+                                  data=fp,
+                                  headers=headers)
             print(f"{file_path} successfully uploaded!") if r.ok else print("Oh no! something went wrong")
+            return r
+
+    def upload_car(self, source_path=None, output_path=None, filename=None):
+        """Store files using Web3.Storage. 
+        You can upload either a single file or multiple files.
+
+        Args:
+            source_path (str): path to file to upload
+            output_path (str): path to car file
+            filename (str): filename that will appear in web3.storage
+        """
+        if source_path is None:
+            print("You need to supply a path")
+
+        if not Path(os.path.expanduser(source_path)).exists():
+            print(f"{source_path} does not exist. Please check you entered the correct path")
+        else:
+            car_dict = create_car(source_path, output_path)
+
+            # supply a filename
+            headers = {"X-NAME": filename.encode(encoding='utf-8')}
+
+            #data = {"file": open(file_path, "rb")}
+            with open(car_dict['output'], "rb") as fp:
+                r = requests.post(self._endpoint + '/car',
+                                  auth=self._bearer_auth,
+                                  data=fp,
+                                  headers=headers)
+            print(f"{output_path} successfully uploaded!") if r.ok else print("Oh no! something went wrong")
             return r
 
     def retrieve(self, cid=None):
